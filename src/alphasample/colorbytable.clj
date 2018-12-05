274fd90785d5516fe17e3665d7bdeeb52beedd49
Teodor Heggelund <teodor@teodorheggelund.com>
2018-12-05 20:48:53 +0100

    Complete step 3

--- src/alphasample/colorbytable.clj
+++ src/alphasample/colorbytable.clj
@@ -2,7 +2,15 @@
   (:require
    [clojure.string :as string]
    [clojure.pprint :refer [pprint]]
+   [clojure.java.io :as io]
+   [clojure.java.shell :as shell]
+
+   [mikera.image.core :as image]
+
+   [alphasample.convert :as convert]
    ))
+
+io/copy
 
 ;; Usage:
 ;;  cat resources/output/table.tsv | clj -m alphasample.colorbytable resources/output/modified-images
@@ -50,23 +58,46 @@
                                    last))]
       output-filename)))
 
+(defn color-dots
+  "Color some dots in an image red"
+  [img color-index?]
+  (let [old-pixels (->> (image/get-pixels img)
+                        (mapv convert/color->rgba))
+        new-color (fn [i]
+                    (cond
+                      ;; Match -> red
+                      (color-index? i) [255 0 0 255]
+
+                      ;; No match -> old
+                      :else (get old-pixels i)))]
+    (image/set-pixels img
+                      (->> (range (count old-pixels))
+                           (map new-color)
+                           (map convert/rgba->color)
+                           int-array))))
+
 (defn color-images [output-dir data]
   ;; Ensure that output directory exists
   (clojure.java.io/make-parents (str output-dir "/testfile"))
   (for [[fname fdata] data]
-    ;; A. copy image to where it should be
-
-    ;; Open image
-
-    ;; Color image
-
-    ;; Save image
     (let [output-filename (str output-dir
                                "/"
                                (-> fname
                                    (string/split #"/")
                                    last))]
-      output-filename)))
+      ;; A. copy image to where it should be
+      (io/copy (io/file fname)
+               (io/file output-filename))
+
+      ;; Open image
+      (let [img (image/load-image output-filename)]
+        ;; Color image
+        (color-dots img (set (map :pixelnr fdata)))
+
+        ;; Save image
+        (image/save img output-filename)
+        [:success output-filename]
+        ))))
 
 (comment
   (-> {}
@@ -80,19 +111,22 @@
       )
   )
 
-(->> (slurp "resources/output/smalltable.tsv")
-     string/split-lines
-     (map table-line->report-entry)
-     (reduce collect-image-entry {})
-     (take 1)
-     (sample-output "resources/step-3")
-     )
+(comment
+  ;; This does the work without reading from stdin
+  (->> (slurp "resources/step-2/table.tsv")
+       string/split-lines
+       (map table-line->report-entry)
+       (reduce collect-image-entry {})
+       ;; (take 1)
+       (color-images "resources/step-3")
+       )
+  )
 
 (defn -main [output-folder]
   (->> (line-seq (java.io.BufferedReader *in*))
        (map table-line->report-entry)
        (reduce collect-image-entry {})
-       (sample-output output-folder)
+       (color-images output-folder)
        ))
 
 (defn collect-by-imagepath [entries])
